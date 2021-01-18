a8fed501b70f1846a01b770dbe8bd37b64c263aa
Edgar Gonzalez <edgar_gm6@hotmail.com>
2021-01-17 20:56:14 -0600

    por fin sin bugs

--- mian.js
+++ mian.js
@@ -1,39 +1,226 @@
-function BinarySearchTree() {
-  this._root = null;
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
 }
-var node = {
-  value: value,
-  left: null,
-  right: null,
-};
-BinarySearchTree.prototype = {
-  constructor: BinarySearchTree,
 
-  insert: function (node, current) {
-    if (node.value < current.value) {
-      if (current.left === null) {
-        current.left = node;
-      } else {
-        this.insert(node, current.left);
-      }
-    } else if (node.value > current.value) {
-      if (current.right === null) {
-        current.right = node;
-      } else {
-        this.insert(node, current.right);
-      }
-    }
-  },
-  add: function (value) {
-    var node = {
-      value: value,
-      left: null,
-      right: null,
-    };
-    if (this._root === null) {
-      this._root = node;
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  add(data) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(data);
+      return;
     } else {
-      this.insert(node, this._root);
+      const searchTree = function (node) {
+        if (data < node.data) {
+          if (node.left === null) {
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      return searchTree(node);
     }
-  },
-};
+  }
+  findMin() {
+    let current = this.root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+  findMax() {
+    let current = this.root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
+  }
+  find(data) {
+    let current = this.root;
+    while (current.data !== data) {
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+      if (current === null) {
+        return null;
+      }
+    }
+    return current;
+  }
+  isPresent(data) {
+    let current = this.root;
+    while (current) {
+      if (data === current.data) {
+        return true;
+      }
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+  remove(data) {
+    const removeNode = function (node, data) {
+      if (node == null) {
+        return null;
+      }
+      if (data == node.data) {
+        // EL NODO NO TIENE HIJOS
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        // EL NODO NO TIENE HIJO IZQUIERDO
+        if (node.left == null) {
+          return node.right;
+        }
+        // EL NODO NO TIENE HIJO DERECHO
+        if (node.right == null) {
+          return node.left;
+        }
+        // EL NODO TIENE DOS HIJOS
+        var tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.root = removeNode(this.root, data);
+  }
+  isBalanced() {
+    return this.findMinHeight() >= this.findMaxHeight() - 1;
+  }
+  findMinHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+    if (left < right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  findMaxHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+    if (left > right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  inOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traverseInOrder(node) {
+        node.left && traverseInOrder(node.left);
+        result.push(node.data);
+        node.right && traverseInOrder(node.right);
+      }
+      traverseInOrder(this.root);
+      return result;
+    }
+  }
+  preOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePreOrder(node) {
+        result.push(node.data);
+        node.left && traversePreOrder(node.left);
+        node.right && traversePreOrder(node.right);
+      }
+      traversePreOrder(this.root);
+      return result;
+    }
+  }
+  postOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePostOrder(node) {
+        node.left && traversePostOrder(node.left);
+        node.right && traversePostOrder(node.right);
+        result.push(node.data);
+      }
+      traversePostOrder(this.root);
+      return result;
+    }
+  }
+
+  levelOrder() {
+    let result = [];
+    let Q = [];
+    if (this.root != null) {
+      Q.push(this.root);
+      while (Q.length > 0) {
+        let node = Q.shift();
+        result.push(node.data);
+        if (node.left != null) {
+          Q.push(node.left);
+        }
+        if (node.right != null) {
+          Q.push(node.right);
+        }
+      }
+      return result;
+    } else {
+      return null;
+    }
+  }
+}
+
+let app = new BinarySearchTree();
+
+app.add(9);
+app.add(4);
+app.add(17);
+app.add(3);
+app.add(6);
+app.add(22);
+app.add(5);
+app.add(7);
+app.add(20);
+app.add(10);
+console.log("Arbol en orden: " + app.levelOrder());
+console.log("Recorrido inOrder: " + app.inOrder());
+console.log("Recorrido preOrder: " + app.preOrder());
+console.log("Recorrido postOrder: " + app.postOrder());
